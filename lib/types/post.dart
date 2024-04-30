af52eb174c99092a21b0664ff9df777fa9a08b96
andra6e <diegoandra6e@gmail.com>
2024-04-29 22:31:45 -0600

    first commit

--- lib/types/post.dart
+++ lib/types/post.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final welcome = welcomeFromJson(jsonString);
-
 import 'dart:convert';
 
 List<Welcome> welcomeFromJson(String str) => List<Welcome>.from(json.decode(str).map((x) => Welcome.fromJson(x)));
